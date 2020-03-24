2cb2b3ad5594ca5442d9e59a4341af9651f87b80
JojoS62 <sn2010@onlinehome.de>
2020-03-24 11:34:36 +0100

    add encoder functions

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 #include "Encoder.h"
 #include "USBMouseKeyboard.h"
 
-DigitalOut led(PC_13);
-Encoder encoder1(PB_7, PB_8, PB_9);
+#define ThreshMax   (40+20)
+#define ThreshMin   (40-20)
+
+DigitalOut  led(PC_13);
+Encoder     encoder1(PB_7, PB_8, PB_9);
+AnalogIn    xRaw(A0);
+AnalogIn    yRaw(A1);
 
 USBMouseKeyboard hid;
 
@@ -16,17 +21,41 @@ int main() {
         encoder1.read(delta, btnPressed, btnReleased);     
 
         if (btnPressed) {
-            led = 0;
-            hid.mediaControl(KEY_MUTE);
-            wait_ms(100);
-            led = 1;
+            led = !led;
+            //hid.mediaControl(KEY_MUTE);
         }   
 
-        if (delta > 0)
-            hid.mediaControl(KEY_VOLUME_UP);
+        if (delta > 0) {
+            if (led)
+                hid.mediaControl(KEY_VOLUME_UP);
+            else
+                hid.move(+5, 0);
+        }
 
-        if (delta < 0)
-            hid.mediaControl(KEY_VOLUME_DOWN);
+        if (delta < 0) {
+            if (led)
+                hid.mediaControl(KEY_VOLUME_DOWN);
+            else
+                hid.move(-5, 0);
+        }
+
+        int x = xRaw * 100.0f;
+        int y = yRaw * 100.0f;
+        
+        if (x < ThreshMin) {
+            hid.move(-5, 0);
+        } else 
+        if (x > ThreshMax) {
+            hid.move(+5, 0);
+        }
+
+        if (y < ThreshMin) {
+            hid.move(0, -5);
+        } else 
+        if (y > ThreshMax)
+        {
+            hid.move(0, +5);
+        }
 
         wait_ms(10);
     }
